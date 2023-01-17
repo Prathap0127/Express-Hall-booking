7eac3b68bf36b885ce2522cd9fc01bcb7407af0d
Prathap Kumaravel <50357476+Prathap0127@users.noreply.github.com>
2023-01-17 17:11:14 +0530

    small change

--- index.js
+++ index.js
@@ -4,62 +4,63 @@ const app = express()
 app.use(express.json())
 const port = 8000
 
-let room = [{
-    id: "001",
-    roomName: "Executive 1",
-    bookedStatus: "Free",
-    noOfSeatAvailable: 30,
-    amenitiesInRoom: "swiming pool , Garden",
-    pricePerhour: 1200
-},
-{
-    id: "002",
-    roomName: "Executive 2",
-    bookedStatus: "Free",
-    noOfSeatAvailable: 37,
-    amenitiesInRoom: "swiming pool , Garden",
-    pricePerhour: 1500
-}
+let room = [
+    // {
+//     id: "001",
+//     roomName: "Executive 1",
+//     bookedStatus: "Free",
+//     noOfSeatAvailable: 30,
+//     amenitiesInRoom: "swiming pool , Garden",
+//     pricePerhour: 1200
+// },
+// {
+//     id: "002",
+//     roomName: "Executive 2",
+//     bookedStatus: "Free",
+//     noOfSeatAvailable: 37,
+//     amenitiesInRoom: "swiming pool , Garden",
+//     pricePerhour: 1500
+// }
 ]
 let customer = [
-    {
-        id: "01",
-        customerName: "prathap",
-        customerEmail: "prathap@gmail.com",
-        customerPhone: "8012170509"
+    // {
+    //     id: "01",
+    //     customerName: "prathap",
+    //     customerEmail: "prathap@gmail.com",
+    //     customerPhone: "8012170509"
 
-    },
-    {
-        id: "02",
-        customerName: "yuvaraj",
-        customerEmail: "yuva@gmail.com",
-        customerPhone: "8012170509"
+    // },
+    // {
+    //     id: "02",
+    //     customerName: "yuvaraj",
+    //     customerEmail: "yuva@gmail.com",
+    //     customerPhone: "8012170509"
 
-    }
+    // }
 ]
 
 let bookingRoom = [
-    {
-        userId: "01",
-        roomId: "001",
-        date: "12.01.2023",
-        startDate: "13.04.2023",
-        endDate: "14.04.2023"
-    },
-    {
-        userId: "02",
-        roomId: "001",
-        date: "12.01.2023",
-        startDate: "13.03.2023",
-        endDate: "14.03.2023"
-    },
-    {
-        userId: "02",
-        roomId: "002",
-        date: "12.01.2023",
-        startDate: "15.03.2023",
-        endDate: "17.03.2023"
-    }
+    // {
+    //     userId: "01",
+    //     roomId: "001",
+    //     date: "12.01.2023",
+    //     startDate: "13.04.2023",
+    //     endDate: "14.04.2023"
+    // },
+    // {
+    //     userId: "02",
+    //     roomId: "001",
+    //     date: "12.01.2023",
+    //     startDate: "13.03.2023",
+    //     endDate: "14.03.2023"
+    // },
+    // {
+    //     userId: "02",
+    //     roomId: "002",
+    //     date: "12.01.2023",
+    //     startDate: "15.03.2023",
+    //     endDate: "17.03.2023"
+    // }
 ]
 
 
@@ -102,7 +103,7 @@ app.post('/room-booking', (req, res) => {
 app.get('/booked-room', (req, res) => {
     let showRooms = [];
 
-    room.forEach(element => {
+    room.map(element => {
 
         console.log(element.id)
         filterbooking = bookingRoom.filter((e) => e.roomId === element.id)
